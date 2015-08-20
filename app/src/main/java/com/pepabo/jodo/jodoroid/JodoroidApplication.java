8af1e6c1d73a2c06cebc9eaa67abc41be663024c
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-08-20 16:08:11 +0900

    APIをたたく枠組みを導入

--- app/src/main/java/com/pepabo/jodo/jodoroid/JodoroidApplication.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/JodoroidApplication.java
@@ -5,17 +5,41 @@ import android.content.Context;
 
 import com.squareup.picasso.Picasso;
 
+
+import com.pepabo.jodo.jodoroid.models.APIService;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+import retrofit.RestAdapter;
+import retrofit.android.AndroidLog;
+import retrofit.client.OkClient;
+
 public class JodoroidApplication extends Application {
+    APIService mService;
     Picasso mPicasso;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mPicasso = createPicasso(getApplicationContext());
+        mService = createService();
     }
 
     public Picasso getPicasso() {
         return mPicasso;
+    }
+
+    public APIService getAPIService() {
+        return mService;
     }
 
     private static Picasso createPicasso(Context context) {
@@ -23,5 +47,51 @@ public class JodoroidApplication extends Application {
                 .loggingEnabled(true)
                 .indicatorsEnabled(BuildConfig.DEBUG)
                 .build();
+    }
+
+    private static APIService createService() {
+        try {
+            final SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new X509TrustManager[]{new TrustEveryoneX509TrustManager()}, null);
+
+            final OkHttpClient client = new OkHttpClient();
+            client.setSslSocketFactory(sslContext.getSocketFactory()); // XXX
+            client.setHostnameVerifier(new NonVerifyingHostnameVerifier()); // XXX
+
+            final RestAdapter adapter = new RestAdapter.Builder()
+                    .setEndpoint("https://157.7.190.186/api/")
+                    .setClient(new OkClient(client))
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .setLog(new AndroidLog("API"))
+                    .build();
+
+            return adapter.create(APIService.class);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class TrustEveryoneX509TrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            throw new CertificateException();
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    private static class NonVerifyingHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/pepabo/jodo/jodoroid/models/APIService.java
@@ -0,0 +1,23 @@
+package com.pepabo.jodo.jodoroid.models;
+
+import java.util.List;
+
+import retrofit.http.DELETE;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import rx.Observable;
+
+public interface APIService {
+    @POST("/login")
+    @FormUrlEncoded
+    Observable<Void> login(@Field("session[email]") String email,
+                           @Field("session[password]") String password);
+
+    @DELETE("/logout")
+    Observable<Void> logout();
+
+    @GET("/feed")
+    Observable<List<Micropost>> fetchFeed();
+}
