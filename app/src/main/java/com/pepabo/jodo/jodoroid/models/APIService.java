6d41d4fbcef53443c7493211827c8b0630bc9c2e
Joe-noh <goflb.jh@gmail.com>
2015-08-27 15:13:24 +0900

    APIサーバと通信するメソッドを増量

--- app/src/main/java/com/pepabo/jodo/jodoroid/models/APIService.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/models/APIService.java
@@ -6,18 +6,73 @@ import retrofit.http.DELETE;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.Multipart;
+import retrofit.http.PATCH;
 import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Part;
+import retrofit.http.Path;
+import retrofit.http.Query;
+import retrofit.mime.TypedFile;
 import rx.Observable;
 
 public interface APIService {
     @POST("/login")
     @FormUrlEncoded
-    Observable<Session> login(@Field("session[email]") String email,
-                           @Field("session[password]") String password);
+    Observable<Session> login(@Field("session[email]")    String email,
+                              @Field("session[password]") String password);
 
     @DELETE("/logout")
     Observable<Void> logout();
 
+    @GET("/users")
+    Observable<List<User>> fetchAllUsers();
+
+    @POST("/users")
+    @FormUrlEncoded
+    Observable<Void> signup(@Field("user[name]")     String name,
+                            @Field("user[email]")    String email,
+                            @Field("user[password]") String password);
+
+
+    @GET("/users/{id}")
+    Observable<User> fetchUser(@Path("id") Integer userId);
+
+    @GET("/users/me")
+    Observable<User> fetchMe();
+
+    @PATCH("/users/me")
+    Observable<User> updateMe();
+
+    @POST("/users/{id}/follow")
+    Observable<Void> followUser(@Path("id") Integer userId);
+
+    @DELETE("/users/{id}/follow")
+    Observable<Void> unfollowUser(@Path("id") Integer userId);
+
+    @GET("/users/{id}/following")
+    Observable<List<User>> fetchFollowing(@Path("id")    Integer userId);
+
+    @GET("/users/{id}/followers")
+    Observable<List<User>> fetchFollowers(@Path("id")    Integer userId);
+
+    @Multipart
+    @POST("/microposts")
+    Observable<Micropost> createMicropost(@Part("micropost[content]") String content,
+                                          @Part("micropost[picture]") TypedFile picture);
+
+    @DELETE("/microposts/{id}")
+    Observable<Void> deleteMicropost(@Path("id") Integer micropostId);
+
     @GET("/feed")
     Observable<List<Micropost>> fetchFeed();
+
+    @POST("/password_resets")
+    @FormUrlEncoded
+    Observable<Void> requestPasswordReset(@Field("password_reset[email]") String email);
+
+    @PUT("/password_resets")
+    @FormUrlEncoded
+    Observable<Session> resetPassword(@Field("user[password]") String password,
+                                      @Query("email")          String email);
 }
