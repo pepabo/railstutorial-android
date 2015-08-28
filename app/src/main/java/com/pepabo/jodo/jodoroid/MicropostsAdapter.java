946a6578fa77937ae0b8c1b4c8666df5982811cf
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-08-28 12:17:25 +0900

    フィード一覧に添付画像を表示する

--- app/src/main/java/com/pepabo/jodo/jodoroid/MicropostsAdapter.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/MicropostsAdapter.java
@@ -1,6 +1,7 @@
 package com.pepabo.jodo.jodoroid;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import com.pepabo.jodo.jodoroid.models.Micropost;
 import com.pepabo.jodo.jodoroid.models.User;
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
@@ -34,7 +36,7 @@ class MicropostsAdapter extends ArrayAdapter<Micropost> {
         final Micropost micropost = getItem(position);
 
         final User user = micropost.getUser();
-        if(user != null) {
+        if (user != null) {
             mPicasso.load(user.getAvatarUrl()).fit().into((ImageView) view.findViewById(R.id.avatar));
             ((TextView) view.findViewById(R.id.username)).setText(user.getName());
         } else {
@@ -44,6 +46,18 @@ class MicropostsAdapter extends ArrayAdapter<Micropost> {
 
         ((TextView) view.findViewById(R.id.content)).setText(micropost.getContent());
         ((TextView) view.findViewById(R.id.timestamp)).setText(formatDate(micropost.getCreatedAt()));
+
+        if (micropost.getPictureUrl() != null) {
+            // Workaround
+            final Uri uri = Uri.parse(URI.create(JodoroidApplication.ENDPOINT)
+                    .resolve(micropost.getPictureUrl().toString()).toString());
+
+            mPicasso.load(uri)
+                    .resize(400, 400).onlyScaleDown().centerInside()
+                    .into((ImageView) view.findViewById(R.id.picture));
+        } else {
+            ((ImageView) view.findViewById(R.id.picture)).setImageDrawable(null);
+        }
 
         return view;
     }
