9143f967f9f26479e4ae39547b105c18925c2e4e
Joe-noh <goflb.jh@gmail.com>
2015-08-31 15:24:39 +0900

    EmailValidatorを追加

---
+++ app/src/main/java/com/pepabo/jodo/jodoroid/EmailValidator.java
@@ -0,0 +1,39 @@
+package com.pepabo.jodo.jodoroid;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    static Pattern REGEXP = Pattern.compile("\\A[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\\z/");
+
+    private String mEmail;
+    private String mError = "";
+
+    public EmailValidator(String email) {
+        mEmail = email;
+    }
+
+    public void validate(Context context) {
+        mError = "";
+
+        if (TextUtils.isEmpty(mEmail)) {
+            mError = context.getString(R.string.error_field_required);
+        } else {
+            Matcher matcher = REGEXP.matcher(mEmail);
+            if (!matcher.matches()) {
+                mError = context.getString(R.string.error_invalid_email);
+            }
+        }
+    }
+
+    public boolean hasError() {
+        return !TextUtils.isEmpty(mError);
+    }
+
+    public String getErrorMessage() {
+        return mError;
+    }
+}
