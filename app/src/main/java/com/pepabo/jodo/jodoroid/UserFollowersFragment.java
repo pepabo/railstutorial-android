6ac511438203cff2b1e1bdeae05ab814de551b5c
Joe-noh <goflb.jh@gmail.com>
2015-08-31 12:26:25 +0900

    followingとfollowersをAPIサーバから取得して表示

--- app/src/main/java/com/pepabo/jodo/jodoroid/UserFollowersFragment.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/UserFollowersFragment.java
@@ -1,8 +1,16 @@
 package com.pepabo.jodo.jodoroid;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.pepabo.jodo.jodoroid.dummy.DummyContent;
+import com.pepabo.jodo.jodoroid.models.APIService;
+import com.pepabo.jodo.jodoroid.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class UserFollowersFragment extends UserListFragment {
     public static final int TYPE_FOLLOWERS = 0;
@@ -29,15 +37,47 @@ public class UserFollowersFragment extends UserListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        switch (getArguments().getInt(ARG_USER_ID)) {
+        long userId = getArguments().getLong(ARG_USER_ID);
+        Observer<List<User>> observer = new Observer<List<User>>() {
+            @Override
+            public void onCompleted() {}
+
+            @Override
+            public void onError(Throwable e) {
+                showErrorToast(e);
+                setUsers(new ArrayList<User>());
+            }
+
+            @Override
+            public void onNext(List<User> users) {
+                setUsers(users);
+            }
+        };
+
+        switch (getArguments().getInt(ARG_TYPE)) {
             case TYPE_FOLLOWERS:
-                setUsers(DummyContent.getFollowers(getArguments().getLong(ARG_USER_ID)));
+                getAPIService()
+                        .fetchFollowers(userId, 1)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(observer);
                 break;
             case TYPE_FOLLOWING:
-                setUsers(DummyContent.getFollowing(getArguments().getLong(ARG_USER_ID)));
+                getAPIService()
+                        .fetchFollowing(userId, 1)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(observer);
                 break;
             default:
                 assert false;
         }
+    }
+
+    private APIService getAPIService() {
+        return ((JodoroidApplication) getActivity().getApplication()).getAPIService();
+    }
+
+    private void showErrorToast(Throwable e) {
+        String message = getString(R.string.toast_load_failure);
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 }
