78655283e657f6f9c2cc537a7ab6e082166d0628
Joe-noh <goflb.jh@gmail.com>
2015-09-02 18:31:26 +0900

    削除処理を実装

--- app/src/main/java/com/pepabo/jodo/jodoroid/MicropostListFragment.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/MicropostListFragment.java
@@ -1,19 +1,22 @@
 package com.pepabo.jodo.jodoroid;
 
-import android.content.Intent;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.pepabo.jodo.jodoroid.models.Micropost;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
 
 /**
  * A fragment representing a list of Items.
@@ -37,24 +40,20 @@ public class MicropostListFragment extends SwipeRefreshListFragment {
         this.getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.e("EEE", "Long Clicked");
-
-                Micropost m = (Micropost) parent.getItemAtPosition(position);
+                final Micropost m = (Micropost) parent.getItemAtPosition(position);
                 long ownerId = m.getUser().getId();
-                
-                new AlertDialog.Builder(getActivity())
-                        .setTitle(R.string.title_delete_micropost)
-                        .setMessage(R.string.description_delete_micropost)
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.e("EEE", "DELETE !!!");
-                            }
-                        })
-                        .setNegativeButton(R.string.cancel, null)
-                        .show();
-                Log.e("EEE", m.getContent());
-                return true;
+
+                if (JodoAccounts.isMe(getActivity().getApplicationContext(), ownerId)) {
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle(R.string.title_delete_micropost)
+                            .setMessage(R.string.description_delete_micropost)
+                            .setPositiveButton(R.string.ok, new PositiveButtonClickListener(m))
+                            .setNegativeButton(R.string.cancel, null)
+                            .show();
+                    return true;
+                } else {
+                    return false;
+                }
             }
         });
     }
@@ -83,6 +82,45 @@ public class MicropostListFragment extends SwipeRefreshListFragment {
             intent.setAction(MainActivity.ACTION_VIEW_USER_PROFILE);
             intent.putExtra(MainActivity.EXTRA_USER_ID, micropost.getUser().getId());
             startActivity(intent);
+        }
+    }
+
+    private class PositiveButtonClickListener implements DialogInterface.OnClickListener {
+        private Micropost mMicropost;
+
+        PositiveButtonClickListener(Micropost m) {
+            mMicropost = m;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            ((JodoroidApplication) getActivity().getApplication()).getAPIService()
+                    .deleteMicropost(mMicropost.getId())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<Void>() {
+
+                        @Override
+                        public void onCompleted() {
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Toast.makeText(
+                                    getActivity(),
+                                    R.string.toast_deletion_failed,
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        }
+
+                        @Override
+                        public void onNext(Void v) {
+                            Toast.makeText(
+                                    getActivity(),
+                                    R.string.toast_deletion_succeed,
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        }
+                    });
         }
     }
 }
