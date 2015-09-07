7349af7b6863006bd05579ab3b21d78f08b26201
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-09-07 11:08:26 +0900

    PasswordChangeActivityでPresenterを呼び出すようにする

--- app/src/main/java/com/pepabo/jodo/jodoroid/PasswordChangeActivity.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/PasswordChangeActivity.java
@@ -6,16 +6,51 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PasswordChangeActivity extends AppCompatActivity {
+import com.pepabo.jodo.jodoroid.models.APIService;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class PasswordChangeActivity extends AppCompatActivity implements PasswordChangeView {
+    APIService mAPIService;
+    PasswordChangePresenter mPresenter;
+
+    ProgressToggle mProgressToggle;
+
+    @Bind(R.id.password)
+    TextView mPasswordView;
+
+    @Bind(R.id.form)
+    View mFormView;
+
+    @Bind(R.id.progress)
+    View mProgressView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_change);
+        ButterKnife.bind(this);
+
+        mProgressToggle = new ProgressToggle(this, mProgressView, mFormView);
 
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+
+        mAPIService = ((JodoroidApplication) getApplication()).getAPIService();
+        mPresenter = new PasswordChangePresenter(getApplicationContext(), mAPIService);
+        mPresenter.setView(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mPresenter.setView(null);
+        super.onDestroy();
     }
 
     @Override
@@ -38,7 +73,31 @@ public class PasswordChangeActivity extends AppCompatActivity {
     }
 
     private void updatePassword() {
-        // TODO: do your work
-        finish();
+        mPresenter.submit();
+    }
+
+    @Override
+    public void setPasswordError(String message) {
+        mPasswordView.setError(message);
+    }
+
+    @Override
+    public String getPassword() {
+        return mPasswordView.getText().toString();
+    }
+
+    @Override
+    public void setPassword(String value) {
+        mPasswordView.setText(value);
+    }
+
+    @Override
+    public void showProgress(boolean show) {
+        mProgressToggle.showProgress(show);
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        Toast.makeText(this, ErrorUtils.getMessage(e), Toast.LENGTH_SHORT).show();
     }
 }
