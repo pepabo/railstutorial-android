fc3218d6f229f27a35421c4b84e089e2a9087eea
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-09-09 15:10:45 +0900

    DIする

--- app/src/main/java/com/pepabo/jodo/jodoroid/JodoroidApplication.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/JodoroidApplication.java
@@ -7,46 +7,37 @@ import android.app.Application;
 import android.content.Intent;
 import android.content.IntentFilter;
 
-import com.pepabo.jodo.jodoroid.modules.APIModule;
-import com.pepabo.jodo.jodoroid.modules.HttpModule;
-import com.pepabo.jodo.jodoroid.modules.PicassoModule;
-import com.squareup.picasso.Picasso;
-
-
 import com.pepabo.jodo.jodoroid.models.APIService;
-import com.squareup.okhttp.OkHttpClient;
-
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import com.pepabo.jodo.jodoroid.modules.AndroidModule;
+import com.squareup.picasso.Picasso;
 
 public class JodoroidApplication extends Application implements OnAccountsUpdateListener {
 
     public static final String ACTION_LOGGED_OUT = "com.pepabo.jodo.jodoroid.LOGGED_OUT";
 
-    APIService mService;
-    Picasso mPicasso;
+    ApplicationComponent mComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        try {
-            final OkHttpClient httpClient = HttpModule.provideHttpClient(this);
-            mPicasso = PicassoModule.createPicasso(this, httpClient);
-            mService = APIModule.createAPIService(this, httpClient);
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            throw new RuntimeException(e);
-        }
+        mComponent = DaggerApplicationComponent.builder()
+                .androidModule(new AndroidModule(this))
+                .build();
 
         AccountManager.get(this).addOnAccountsUpdatedListener(this, null, true);
     }
 
+    public ApplicationComponent component() {
+        return mComponent;
+    }
+
     public Picasso getPicasso() {
-        return mPicasso;
+        return component().picasso();
     }
 
     public APIService getAPIService() {
-        return mService;
+        return component().apiService();
     }
 
     @Override
@@ -54,12 +45,12 @@ public class JodoroidApplication extends Application implements OnAccountsUpdate
         boolean found = false;
 
         for (Account account : accounts) {
-            if(account.type.equals(JodoAuthenticator.ACCOUNT_TYPE)) {
+            if (account.type.equals(JodoAuthenticator.ACCOUNT_TYPE)) {
                 found = true;
             }
         }
 
-        if(!found) {
+        if (!found) {
             sendBroadcast(new Intent(ACTION_LOGGED_OUT));
         }
     }
