9f56f144bd02e0fe596953bee839ee9dde0221b0
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-09-17 15:47:26 +0900

    プロフィール変更時に，drawerの表示を更新する

---
+++ app/src/main/java/com/pepabo/jodo/jodoroid/AccountInfoView.java
@@ -0,0 +1,9 @@
+package com.pepabo.jodo.jodoroid;
+
+import android.net.Uri;
+
+import com.pepabo.jodo.jodoroid.models.User;
+
+public interface AccountInfoView {
+    void setAccountUser(User value);
+}
