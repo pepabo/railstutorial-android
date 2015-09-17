85664fd5fea97264bcd9b3fb1d1b14896701991c
kojikoji <kojima.t@pepabo.com>
2015-09-17 11:56:41 +0900

    リフレッシュ中の表示が消えなくなってしまっていたのを修正

--- app/src/main/java/com/pepabo/jodo/jodoroid/RefreshPresenter.java
+++ app/src/main/java/com/pepabo/jodo/jodoroid/RefreshPresenter.java
@@ -102,6 +102,7 @@ public abstract class RefreshPresenter<Model> {
             final RefreshableView<Model> view = getView();
             if (view != null) {
                 view.onLoadError(e);
+                view.setRefreshing(false);
             }
         }
     }
